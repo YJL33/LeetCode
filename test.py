c49539bceca2ff105b987e10bc9312de17ce3218
YJL33 <yunjun.l33@gmail.com>
2016-03-04 00:04:51 -0800

    Solving #37 (result ok but Type Error in Line 25)

--- test.py
+++ test.py
@@ -3,20 +3,27 @@ General testing model for LEETCODE
 """
 import sys
 #import _264_ugly_numbers_2 as mod
-import _36_valid_sudoku as mod
+import _36_valid_sudoku as mod36
+import _37_sudoku_solver as mod37
 
-board = ["..5.....6","....14...",".........",".....92..","5....2...",".......3.","...54....","3.....42.","...27.6.."]
+#board = ["53..7....","6..195...",".98....6.","8...6...3","4..8.3..1","7...2...6",".6....28.","...419..5","....8..79"]
+board = ["..9748...","7........",".2.1.9...","..7...24.",".64.1.59.",".98...3..","...8.3.2.","........6","...2759.."]
+sol36 = mod36.Solution()
+print mod36.Solution.isValidSudoku(sol36, board)
+print board
+
+sol37 = mod37.Solution()
+board2 = mod37.Solution.solveSudoku(sol37, board)
+print board2
+
 """
-[["5", "3", ".", ".", "7", ".", ".", ".", "."],
-		 ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-		 [".", "9", "8", ".", ".", ".", ".", "6", "."],
-		 ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-		 ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-		 ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-		 [".", "6", ".", ".", ".", ".", "2", "8", "."],
-		 [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-		 [".", ".", ".", ".", "8", ".", ".", "7", "9"],]
-		 """
-sol = mod.Solution()
-sol.isValidSudoku = mod.Solution.isValidSudoku(sol, board)
-print sol.isValidSudoku
+print board2[0]
+print board2[1]
+print board2[2]
+print board2[3]
+print board2[4]
+print board2[5]
+print board2[6]
+print board2[7]
+print board2[8]
+"""

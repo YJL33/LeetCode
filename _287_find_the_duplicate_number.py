d5d9c84fa653086adeb5811e5676e37db1910114
YJL33 <yunjun.l33@gmail.com>
2016-04-27 20:12:22 -0700

    Solved #287

--- _287_find_the_duplicate_number.py
+++ _287_find_the_duplicate_number.py
@@ -17,4 +17,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        try:
+            slow = nums[0]
+            fast = nums[nums[0]]
+            while slow is not fast:
+                slow = nums[slow]
+                fast = nums[nums[fast]]
+            # Here slow = fast = meeting point (M)
+            # Use two pointers each begin at head and M, they will meet at entry.
+            # print "fast", fast, "slow", slow
+            entry = 0
+            while slow is not entry:
+                slow = nums[slow]
+                entry = nums[entry]
+            return entry
+        except:
+            return None
+
+# This solution failed on 1 test sample, which has correct output in local environment.

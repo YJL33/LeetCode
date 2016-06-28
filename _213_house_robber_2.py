1a88a5ad16aa068dc16eb0df7b68c2cb415c3c6b
YJL33 <yunjun.l33@gmail.com>
2016-06-27 21:32:45 -0700

    Solved #213

---
+++ _213_house_robber_2.py
@@ -0,0 +1,20 @@
+"""
+213. House Robber II
+
+Note: This is an extension of House Robber.
+
+After robbing those houses on that street,
+the thief has found himself a new place for his thievery so that he won't get too much attention.
+This time, all houses at this place are arranged in a circle.
+That means the first house is the neighbor of the last one.
+Meanwhile, the security system for these houses is as same as for those in the previous street.
+
+Given a list of non-negative integers representing the amount of money of each house,
+determine the maximum amount of money you can rob tonight without alerting the police.
+"""
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """

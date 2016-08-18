b79ca580142c4244452d42b5dba4c4b0540365cd
YJL33 <yunjun.l33@gmail.com>
2016-08-18 15:08:55 -0700

    Solved #133 and #332. (graph problems)

---
+++ _133_clone_graph_DFS.py
@@ -0,0 +1,57 @@
+"""
+133. Clone Graph
+
+Clone an undirected graph.
+Each node in the graph contains a label and a list of its neighbors.
+
+OJ's undirected graph serialization:
+
+Nodes are labeled uniquely.
+We use # as a separator for each node, and,
+as a separator for node label and each neighbor of the node.
+
+As an example, consider the serialized graph {0,1,2#1,2#2,2}.
+
+The graph has a total of three nodes, and therefore contains three parts as separated by #.
+
+    First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
+    Second node is labeled as 1. Connect node 1 to node 2.
+    Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
+
+Visually, the graph looks like the following:
+
+       1
+      / \
+     /   \
+    0 --- 2
+         / \
+         \_/
+"""
+# Definition for a undirected graph node
+# class UndirectedGraphNode(object):
+#     def __init__(self, x):
+#         self.label = x
+#         self.neighbors = []
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: UndirectedGraphNode
+        :rtype: UndirectedGraphNode
+        """
+        # Iteratibe DFS
+        if not node: return
+        newnode = UndirectedGraphNode(node.label)
+        dct = {node: newnode}
+        queue = [node]
+        while queue:
+            node = queue.pop()
+            for nb in node.neighbors:
+                if nb in dct:
+                    dct[node].neighbors.append(dct[nb])
+                else:
+                    newnb = UndirectedGraphNode(nb.label)
+                    dct[nb] = newnb
+                    dct[node].neighbors.append(newnb)
+                    queue.append(nb)
+        return newnode
---
+++ _332_reconstruct_itinerary.py
@@ -0,0 +1,41 @@
+"""
+332. Reconstruct Itinerary
+
+Given a list of airline tickets represented by pairs of departure and arrival airports [from, to],
+reconstruct the itinerary in order. All of the tickets belong to a man who departs from JFK.
+Thus, the itinerary must begin with JFK.
+
+Note:
+
+If there are multiple valid itineraries,
+you should return the itinerary that has the smallest lexical order when read as a single string.
+For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+All airports are represented by three capital letters (IATA code).
+You may assume all tickets form at least one valid itinerary.
+
+Example 1:
+tickets = [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+Return ["JFK", "MUC", "LHR", "SFO", "SJC"].
+
+Example 2:
+tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+Return ["JFK","ATL","JFK","SFO","ATL","SFO"].
+Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"].
+But it is larger in lexical order.
+"""
+class Solution(object):
+    def findItinerary(self, tickets):
+        """
+        :type tickets: List[List[str]]
+        :rtype: List[str]
+        """
+        # Iterative solution
+        airports = collections.defaultdict(list)
+        for depart, arrive in sorted(tickets)[::-1]:    # Add smaller lexical airport later
+            airports[depart] += arrive,
+        route, stack = [], ['JFK']
+        while stack:
+            while airports[stack[-1]]:                  # for each airport
+                stack += airports[stack[-1]].pop(),     # pick smallest one, and put it into stack
+            route += stack.pop(),                       # add them into route
+        return route[::-1]

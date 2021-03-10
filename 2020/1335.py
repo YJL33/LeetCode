fa8f1e2f901c9477cc0fde240d16cac14df4d046
YJL33 <yunjun.l33@gmail.com>
2021-03-10 15:28:08 +0800

    Use DP on #1335.

--- 2020/1335.py
+++ 2020/1335.py
@@ -1,24 +1,41 @@
 """
 1335
 """
-import functools
+import collections
 class Solution:
     def minDifficulty(self, A, d):
-        n = len(A)
-        if n < d: return -1
+        if len(A) < d:
+            return -1
+        if len(A) == d:
+            return sum(A)
+        if d == 1:
+            return max(A)
 
-        @functools.lru_cache
-        def dfs(i, d):
-            if d == 1:
-                return max(A[i:])
-            res, maxd = float('inf'), 0
-            for j in range(i, n-d+1):
-                maxd = max(maxd, A[j])
-                res = min(res, maxd + dfs(j+1, d-1))
-            return res
-        return dfs(0, d)
+        # use dp
+        # dp[d][i]: the answer of starting from that index i, d days left
+        # dp[0] is useless
+        # e.g.
+        # dp[6][0] = min(inf, A[0]+dp[5][1])
+        
+        # fill dp
+        L = len(A)
+        dp = [[float('inf') for _ in range(L)]+[0] for _ in range(d+1)]
+
+        for dd in range(1,d+1):
+            # maintain maxD
+            for i in range(L-dd+1):
+                maxD = 0
+                localAns = float('inf')
+                for j in range(i, L-dd+1):
+                    maxD = max(A[j], maxD)
+                    # print('dd, i, j, maxD, dp[dd-1][j+1], LocalAns: ', dd, i, j, maxD, dp[dd-1][j+1], localAns)
+                    localAns = min(localAns, maxD+dp[dd-1][j+1])
+                dp[dd][i] = localAns
+                # print('=== stop updating dp[', dd, '][', i, ']', '===>', dp[dd][i])
+        # print(dp)
+        return dp[d][0] if dp[d][0] < float('inf') else -1
                 
 # print(Solution().minDifficulty([9,9,9], 4))
 # print(Solution().minDifficulty([9,9,9], 3))
 # print(Solution().minDifficulty([6,5,4,3,2,1], 2))
-print(Solution().minDifficulty([11,111,22,222,33,333,44,444], 6))
+print(Solution().minDifficulty([22,222,11,111,33,333,44,444], 7))

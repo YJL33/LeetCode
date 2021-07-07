1c46517cfe09ae8a0530b878779c79fdedb71884
YJL33 <yunjun.l33@gmail.com>
2021-07-07 18:40:25 +0800

    Fix Typo.

--- current_session/17.py
+++ current_session/17.py
@@ -7,7 +7,7 @@ class Solution(object):
         :type digits: str
         :rtype: List[str]
         """
-        cd = {'2':'abc','3':'cde','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+        cd = {'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
         res = []
         for n in digits:
             if not res:

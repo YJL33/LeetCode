a0b61be1293dd42f9a0c5b406994e4d5388e8130
YJL33 <yunjun.l33@gmail.com>
2021-07-08 06:20:44 +0800

    Update #50.

--- current_session/50.py
+++ current_session/50.py
@@ -9,13 +9,15 @@ class Solution(object):
         :rtype: float
         """
         if n == 0: return 1
-        ans, neg = 1, n<0
-        while n:
-            if n%2:
-                ans, n = ans*x, n-1
+        ans, neg = 1, True if n < 0 else False
+        if neg:
+            n, rec = -n, True
+        while n != 1:
+            if n % 2:
+                n, ans = n-1, ans*x
             else:
-                x, n = x*x, n/2
-
+                n, x = n/2, x*x
+        return ans*x if not neg else 1/(ans*x)
 
 print(Solution().myPow(2.00000, 10))
 print(Solution().myPow(2.10000, 3))

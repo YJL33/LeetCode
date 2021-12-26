44c9984deb6649400bd51554dddb3cafc2a461fe
YJL33 <yunjun.l33@gmail.com>
2021-12-26 15:56:16 +0800

    Add #2116.

--- current_session/python/2116.py
+++ current_session/python/2116.py
@@ -1,39 +1,18 @@
-import collections
 class Solution:
     def canBeValid(self, s: str, locked: str) -> bool:
-        # use stack
-        # check each element
-        # if locked and '(': add as '('
-        # if locked and ')': pop, see if can amend or not
-        # if not locked: check the stack if it's possible to negate the previous one
-        dq = collections.deque()
-        i = 0
-        while i < len(s):
-            if locked[i] == '1' and s[i] == '(':
-                dq.append((i, '('))
-            elif locked[i] == '1' and s[i] == ')':
-                if not dq: return False
-                left, type = dq.pop()
-            else:
-                if dq[-1][1] == '(':
-                    dq.pop()
-                else:
-                    dq.append((i, '_'))
-            i += 1
-        
-        print('dq', dq)
-        
-        leftCnt = 0
-        while dq:
-            _, type = dq.popleft()
-            if type == '(':
-                leftCnt += 1
-            else:
-                if leftCnt:
-                    leftCnt -= 1
-                else:
-                    leftCnt += 1
-
-        return leftCnt == 0
+        if len(s) % 2 == 1: return False
+        tot = op = cl = 0 # tot -> Total variable brackets, op -> Open, cl -> Closed
+        for i in range(len(s) - 1, -1, -1):
+            if locked[i] == '0': tot += 1
+            elif s[i] == '(': op += 1
+            elif s[i] == ')': cl += 1
+            if tot - op + cl < 0: return False
+        tot = op = cl = 0
+        for i in range(len(s)):
+            if locked[i] == '0': tot += 1
+            elif s[i] == '(': op += 1
+            elif s[i] == ')': cl += 1
+            if tot + op - cl < 0: return False 
+        return True
 
 print(Solution().canBeValid("())()))()(()(((())(()()))))((((()())(())","1011101100010001001011000000110010100101"), 'should be true')
